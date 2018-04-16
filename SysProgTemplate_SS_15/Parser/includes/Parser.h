d02a613d68e38b8d88a6266683f312e66e51e0a3
medem <me.dem2811@gmail.com>
2018-04-16 21:59:16 +0200

    parser skeleton

--- SysProgTemplate_SS_15/Parser/includes/Parser.h
+++ SysProgTemplate_SS_15/Parser/includes/Parser.h
@@ -1,14 +1,71 @@
 /*
  * Parser.h
  *
+ * The parser analyzes the Syntax of the file and
+ * creates the parsetree
+ *
  *  Created on: 15.04.2018
  *      Author: mella
  */
+#include "../../Scanner/includes/Scanner.h"
 
 #ifndef PARSER_INCLUDES_PARSER_H_
 #define PARSER_INCLUDES_PARSER_H_
 
+class Parser {
 
+public:
+
+	/**
+	 * Constructor and Destructor
+	 */
+	Parser(char* filename);
+	~Parser();
+
+	bool parse();
+
+private:
+
+	Scanner* scanner;
+
+	//functions for all the Grammarrules
+
+	// PROG ::= DECLS STATEMENTS
+	void PROG();
+
+	// DECLS ::= DECL;DECLS | E
+	void DECLS();
+
+	// DECL ::= int ARRAY identifier
+	void DECL();
+
+	// ARRAY ::= [int] | E
+	void ARRAY();
+
+	// STATEMENTS ::= STATEMENT;STATEMENTS | E
+	void STATEMENTS();
+
+	// STATEMENT ::= identifier INDEX:=EXP | write(EXP) |
+	// read(identifier INDEX) | {STATEMENTS} | if (EXP) STATEMENT else STATEMENT |
+	// while (EXP) STATEMENT
+	void STATEMENT();
+
+	// EXP ::= EXP2 OP_EXP
+	void EXP();
+
+	// EXP2 ::= (EXP) | identifier INDEX | integer | -EXP2 | !EXP2
+	void EXP2();
+
+	// INDEX ::= [EXP] | E
+	void INDEX();
+
+	// OP_EXP ::= OP EXP | E
+	void OP_EXP();
+
+	// OP ::= + | - | * | : | < | > | = | =:= | &&
+	void OP();
+
+};
 
 
 
--- SysProgTemplate_SS_15/Parser/src/Parser.cpp
+++ SysProgTemplate_SS_15/Parser/src/Parser.cpp
@@ -6,5 +6,303 @@
  */
 
 
+#include "../includes/Parser.h"
+
+Parser::Parser(char* filename) {
+	scanner = new Scanner(filename);
+}
+
+Parser::~Parser() {
+
+}
+
+bool parse () {
+
+	return true;
+}
+
+void Parser::PROG() {
+	DECLS();
+	STATEMENTS();
+}
+
+void Parser::DECLS() {
+
+	if (1 == 1) { //Token == int
+
+		DECL();
+
+		if (1) { //Token == ;
+
+			DECLS();
+
+		} else { //error
+
+		}
+
+
+	} else { //epsilon
+
+	}
+}
+
+void Parser::DECL() {
+
+	if (1) { //Token == int
+
+		ARRAY();
+
+		if (1) { //Token == identifier
+
+		}
+	} else { //error
+
+	}
+}
+
+void Parser::ARRAY() {
+
+	if (1) { //Token == [
+
+		if (1) { // ==integer
+
+			if (1) { // == ]
+
+			} else { //error
+
+			}
+
+		} else { //error
+
+		}
+
+	} else { //epsilon
+
+	}
+}
+
+void Parser::STATEMENTS() {
+
+	if (1) { // ==identifier | == write | == read | == { | == if | == while
+
+		STATEMENT();
+
+		if(1) { // == ;
+
+			STATEMENTS();
+
+		} else { //error
+
+		}
+
+	} else { //epsilon
+
+	}
+}
+
+void Parser::STATEMENT() {
+
+	if (1) { //identifier
+
+
+		INDEX();
+
+		if (1) { // == :=
+
+
+			EXP();
+
+		} else { // error
+
+		}
+
+	} else if (1) { // write
+
+		if (1) { // (
+
+			EXP();
+
+			if (1) { // )
+
+			} else { //error
+
+			}
+		} else { //error
+
+		}
+
+	} else if (1) { //read
+
+		if (1) { // (
+
+			if (1) { // identifier
+
+				INDEX();
+
+				if (1) { // )
+
+				} else { //error
+
+				}
+			} else { //error
+
+			}
+		} else { //error
+
+		}
+
+	} else if (1) { // {
+
+		STATEMENTS();
+
+		if (1) { // }
+
+		} else { //error
+
+		}
+
+	} else if (1) { // if
+
+		if (1) { // (
+
+			EXP();
+
+			if (1) { // )
+
+				STATEMENT();
+
+				if (1) { // else
+
+					STATEMENT();
+
+				} else { //error
+
+				}
+
+			} else { //error
+
+			}
+
+		} else { //error
+
+		}
+
+	} else if (1) { // while
+
+		if (1) { // (
+
+			EXP();
+
+			if (1) { // )
+
+				STATEMENT();
+
+			} else { //error
+
+			}
+
+		} else { //error
+
+		}
+
+	} else { //error
+
+	}
+}
+
+void Parser::EXP() {
+
+	EXP2();
+
+	OP_EXP();
+}
+
+void Parser::EXP2() {
+
+	if (1) { // (
+
+		EXP();
+
+		if (1) { // )
+
+		} else { //error
+
+		}
+	} else if (1) { //identifier
+
+		INDEX();
+	} else if (1) { //integer
+
+	} else if (1) { //-
+
+		EXP2();
+	} else if (1) { //!
+
+		EXP2();
+	} else { //error
+
+	}
+
+}
+
+void Parser::INDEX() {
+
+	if (1) { // [
+
+		EXP();
+
+		if (1) { // ]
+
+		} else { //error
+
+		}
+
+	} else { //epsilon
+
+	}
+
+}
+
+void Parser::OP_EXP() {
+
+	if (1) { // + | - | * | : | < | > | = | =:= | &&
+
+		OP();
+
+		EXP();
+
+	} else { //epsilon
+
+	}
+
+}
+
+void Parser::OP() {
+
+	if (1) { // +
+
+	} else if (1) { // -
+
+	} else if (1) { // *
+
+	} else if (1) { // :
+
+	} else if (1) { // <
+
+	} else if (1) { // >
+
+	} else if (1) { // =
+
+	} else if (1) { // =:=
+
+	} else if (1) { // &&
+
+	} else { //error
+
+	}
+
+}
+
+
 
 
